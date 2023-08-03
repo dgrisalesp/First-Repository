8b6861f690165cbaba5e8a4579ad69f2fffb7c46
unknown <dgrisalesp@eafit.edu.co>
2023-08-03 16:46:31 -0500

    Cambios en la función About

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render (request,'home.html',{'name':'David Grisales'})
+    return render (request,'home.html',{'name':'David Grisales Posada'})
 def About(request):
     return render (request,"About.html",{'name':"David Grisales Posada"})
