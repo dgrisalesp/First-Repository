1133bf1301908cf6f9cdf41dbe2729ceb01e735b
unknown <dgrisalesp@eafit.edu.co>
2023-08-24 17:01:16 -0500

    Segundo taller

--- news/views.py
+++ news/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def news(request):
-    newss=models.News.objects.all()
+    newss=models.News.objects.all().order_by('-date')
     return render(request,'news/news.html',{'newss':newss})
